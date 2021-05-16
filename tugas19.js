019b75cce1874e28a0ee171ebb9b3531ab5cd819
Faza <firdausauliafaza@gmail.com>
2021-05-16 18:07:02 +0700

    niomic.com

---
+++ tugas19.js
@@ -0,0 +1,20 @@
+function tugas1() {
+  var a = "Semangat...";
+  console.log(a.repeat(5));
+}
+tugas1();
+console.log("");
+
+function tugas2() {
+  var bintang = "*";
+  console.log(bintang.repeat(5));
+  console.log(bintang.repeat(20));
+  console.log(bintang.repeat(40));
+  console.log(bintang.repeat(60));
+  console.log(bintang.repeat(100));
+  console.log(bintang.repeat(60));
+  console.log(bintang.repeat(40));
+  console.log(bintang.repeat(20));
+  console.log(bintang.repeat(5));
+}
+tugas2();
